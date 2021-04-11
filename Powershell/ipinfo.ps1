7bfbb7230f32757ca15aaf225764c7c879047b7e
smithajosephpaikayil <smithajoseph02@gmail.com>
2021-04-10 23:07:31 -0400

    lab3

---
+++ Powershell/ipinfo.ps1
@@ -0,0 +1,9 @@
+get-ciminstance win32_networkadapterconfiguration |
+    Where-Object IPEnabled -eq "True" |
+        Select-Object -Property @{n="Adapter Description";e={$_.description}},
+				@{n="Index";e={$_.index}},
+                                @{n="IP Address";e={$_.ipaddress}},
+                                @{n="Subnet Mask";e={$_.ipsubnet}},
+                                @{n="DNS Domain Name";e={$_.dnshostname}},
+                                @{n="DNS Server"; e={$_.DNSServerSearchOrder}} |
+Format-Table -autosize
